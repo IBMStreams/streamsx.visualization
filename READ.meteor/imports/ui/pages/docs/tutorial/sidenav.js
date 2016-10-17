c2ae345c09192c2fb125f6f093ac2f12f398c024
SRINIVASAN PARTHASARATHY <spartha@us.ibm.com>
2016-10-17 19:38:48 -0400

    advanced usecases

--- READ.meteor/imports/ui/pages/docs/tutorial/sidenav.js
+++ READ.meteor/imports/ui/pages/docs/tutorial/sidenav.js
@@ -1,6 +1,7 @@
-import carsSideNavTemplate from './cars/sidenav.html';
-import moviesSideNavTemplate from './movies/sidenav.html';
-import timeSeriesSideNavTemplate from './timeseries/sidenav.html';
+import './cars/sidenav.html';
+import './movies/sidenav.html';
+import './timeseries/sidenav.html';
+import './advancedusecases/sidenav.html';
 
 export const tutorialSideNavCtrl = ['$scope', 'readState', function ($scope, readState) {
   this.readState = readState;
