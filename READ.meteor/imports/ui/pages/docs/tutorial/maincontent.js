825f4ab8bc8a1f53d13965d41a94d66adf327070
SRINIVASAN PARTHASARATHY <spartha@us.ibm.com>
2016-10-18 09:19:37 -0400

    reactive designer fixes

--- READ.meteor/imports/ui/pages/docs/tutorial/maincontent.js
+++ READ.meteor/imports/ui/pages/docs/tutorial/maincontent.js
@@ -8,6 +8,9 @@ import './timeseries/timeseriescharts.html';
 
 import './dynamicrealtimecharts/dynamiccharts.html';
 
+import './cars/createapp.html';
+import './cars/datacleaning.html';
+
 
 export const tutorialMainContentCtrl = ['$scope', 'readState', '$uibModal', function ($scope, readState, $uibModal) {
   this.readState = readState;
