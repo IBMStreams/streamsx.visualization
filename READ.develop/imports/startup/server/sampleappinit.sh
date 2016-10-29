218016e37e41318f26bf8360fa407ab534429f8e
SRINIVASAN PARTHASARATHY <spartha@us.ibm.com>
2016-10-28 20:53:28 -0400

    READ.develop and README.md changes

--- READ.develop/imports/startup/server/sampleappinit.sh
+++ READ.develop/imports/startup/server/sampleappinit.sh
@@ -1,11 +1,11 @@
 echo 'export default app = ' > sampleappinit.js
-mongoexport -h localhost:3001 -d meteor -c apps --jsonArray --pretty >> sampleappinit.js
+mongoexport -h localhost:3001 -d readdb -c apps --jsonArray --pretty >> sampleappinit.js
 
 echo 'export default dashboards = ' > sampledashboardsinit.js
-mongoexport -h localhost:3001 -d meteor -c dashboards --jsonArray --pretty >> sampledashboardsinit.js
+mongoexport -h localhost:3001 -d readdb -c dashboards --jsonArray --pretty >> sampledashboardsinit.js
 
 echo 'export default datasets = ' > sampledatasetsinit.js
-mongoexport -h localhost:3001 -d meteor -c datasets --jsonArray --pretty >> sampledatasetsinit.js
+mongoexport -h localhost:3001 -d readdb -c datasets --jsonArray --pretty >> sampledatasetsinit.js
 
 echo 'export default visualizations = ' > samplevisualizationsinit.js
-mongoexport -h localhost:3001 -d meteor -c visualizations --jsonArray --pretty >> samplevisualizationsinit.js
+mongoexport -h localhost:3001 -d readdb -c visualizations --jsonArray --pretty >> samplevisualizationsinit.js
